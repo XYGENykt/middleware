9b862fc017cf0c11a62b5380dba560c8c386af3b
XYGENykt <zee@labis.su>
2025-06-01 14:13:02 +0900

    getWeather

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -18,8 +19,11 @@ type WeatherResponse struct {
 	Latitude          float64 `json:"latitude"`
 	Longitude         float64 `json:"longitude"`
 	Timezone          string  `json:"timezone"`
+	Date              string  `json:"datetime"`
 	CurrentConditions struct {
-		Temp       float64 `json:"temp"`
+		TempF      float64 `json:"temp"`
+		TempC      float64 `json:"tempC"`
+		Date       string  `json:"datetime"`
 		Humidity   float64 `json:"humidity"`
 		Conditions string  `json:"conditions"`
 	} `json:"currentConditions"`
@@ -37,7 +41,7 @@ func main() {
 	s := echo.New()
 
 	s.GET("/status", Handler)
-	s.GET("/weather", Weather)
+	s.GET("/weather", getWeather)
 
 	err = s.Start(":8080")
 
@@ -47,26 +51,78 @@ func main() {
 
 }
 
-func Weather(ctx echo.Context) error {
+func getWeather(ctx echo.Context) error {
 	// Формируем URL запроса
 	apiKey := os.Getenv("WEATHER_API_KEY")
-	url := "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Sankt-Peterburg/2022-01-01T15:00:00?key=" + apiKey + "&include=current"
-	fmt.Print(url)
-	// Выполняем GET-запрос
-	resp, err := http.Get(url)
-	if err != nil {
-		return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to fetch weather data"})
-	}
-	defer resp.Body.Close()
+	baseURL := "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Sankt-Peterburg/"
 
-	// Читаем тело ответа
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to read response"})
+	// вытаскиваю все четверги
+	thursdays := getThursdays(2023, 2025)
+
+	// Проверка
+	// thursdays := []string{
+	// 	"2022-01-01",
+	// 	"2022-01-02",
+	// 	"2022-01-03",
+	// }
+
+	// Создаем массив для хранения прогнозов
+	var forecasts []WeatherResponse
+
+	// Цикл с прогнозом для каждой даты
+
+	for _, date := range thursdays {
+		url := baseURL + date + "T15:00:00?key=" + apiKey + "&include=current"
+
+		resp, err := http.Get(url)
+		if err != nil {
+			return ctx.JSON(http.StatusInternalServerError, map[string]string{
+				"error": fmt.Sprintf("Failed to fetch weather data for date %s", date),
+			})
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return ctx.JSON(http.StatusInternalServerError, map[string]string{
+				"error": fmt.Sprintf("Failed to read response for date %s", date),
+			})
+		}
+
+		var weatherData WeatherResponse
+		weatherData.Date = date
+
+		if err := json.Unmarshal(body, &weatherData); err != nil {
+			return ctx.JSON(http.StatusInternalServerError, map[string]string{
+				"error": fmt.Sprintf("Failed to parse weather data for date %s", date),
+			})
+		}
+
+		// Формула переноса сельция в фаренгейт (1 °C × 9/5) + 32 = 33,8 и наоборот (32 °F − 32) × 5/9 = 0 °C
+		weatherData.CurrentConditions.TempC = (weatherData.CurrentConditions.TempF - 32.0) * 5 / 9
+
+		forecasts = append(forecasts, weatherData)
 	}
 
-	// Возвращаем сырой JSON (можно распарсить в структуру WeatherResponse)
-	return ctx.JSONBlob(http.StatusOK, body)
+	return ctx.JSON(http.StatusOK, forecasts)
+}
+
+func getThursdays(startYear, endYear int) []string {
+
+	var thursdays []string
+
+	// Устанавливаем начальную и конечную даты
+	start := time.Date(startYear, time.January, 1, 0, 0, 0, 0, time.UTC)
+	end := time.Date(endYear, time.Now().Month(), 0, 23, 59, 59, 0, time.UTC)
+
+	// Перебираем дни и добавляем четверги в массив
+	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
+		if d.Weekday() == time.Thursday {
+			thursdays = append(thursdays, d.Format("2006-01-02"))
+		}
+	}
+
+	return thursdays
 }
 
 func Handler(ctx echo.Context) error {
@@ -87,22 +143,4 @@ func Handler(ctx echo.Context) error {
 		return (err)
 	}
 	return nil
-}
-
-func getThursdays(startYear, endYear int) []string {
-
-	var thursdays []string
-
-	// Устанавливаем начальную и конечную даты
-	start := time.Date(startYear, time.January, 1, 0, 0, 0, 0, time.UTC)
-	end := time.Date(endYear, time.December, 31, 23, 59, 59, 0, time.UTC)
-
-	// Перебираем дни и добавляем четверги в массив
-	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
-		if d.Weekday() == time.Thursday {
-			thursdays = append(thursdays, d.Format("2006-01-02"))
-		}
-	}
-
-	return thursdays
 }
